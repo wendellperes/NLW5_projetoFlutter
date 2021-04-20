a468833d62af64943ea4a7f4bfa06ca943ba75ab
WendellCosta759 <wendellperes@pop-os.localdomain>
2021-04-20 18:12:36 -0300

    Aula 01

---
+++ lib/home/widgets/chart/chart_widget.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ChartWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 80,
+      width: 80,
+      child: Stack(
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/home/widgets/score_card/score_card_widget.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class ScoreCardWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        children: [
+          Expanded(
+            flex: 1,
+            child: Container(
+              width: 80,
+              height: 80,
+              color: Colors.red,
+            ),
+          ),
+          Expanded(
+            flex: 3,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 24),
+                child: Column(
+                  children: [
+                    Text("Vamos Comecar"),
+                    Text("Complete os desafios e avance em conhecimento"),
+                  ],
+                ),
+              )
+          )
+
+        ],
+      ),
+    );
+  }
+}
